310a90a9244271a06e000922d2d5bdc26d7b7f9e
TheJB4 <juanmer382@gmail.com>
2023-11-28 12:03:14 -0300

    añadiendo el footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header'
-
+import Footer from './Components/Footer'
 function App() {
   return (
     <>
       <Header></Header>
+      <Footer></Footer>
     </>
   )
 }
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,15 +1,37 @@
 import { useState } from 'react'
-import {faBars} from ''
+import { LuMenu } from "react-icons/lu";
 
-function Header(){
-    return(
-        <header className='flex justify-center items-center w-full'>
+
+function Header() {
+    let [openMenu, setOpenMenu] = useState(false)
+
+    let toggle = (e) => {
+        setOpenMenu(!openMenu)
+    }
+
+    return (
+        <header className='flex  justify-end items-center w-full'>
             <div>
                 <img src="" alt="" />
             </div>
             <div>
-                <FontAwesomeIcon icon={faBars}/>
-            </div>  
+                <LuMenu onClick={toggle} />
+                {
+                    openMenu &&
+                    <div >
+                        <ul>
+                            <li>Tracyectoria</li>
+                            <li>Prensa</li>
+                            <li>Pacientes</li>
+                            <li>Articulos</li>
+                            <li>Infloblog</li>
+                            <li>Testimonios</li>
+                            <li>Videos</li>
+                            <li>Contactos</li>
+                        </ul>
+                    </div>
+                }
+            </div>
         </header>
     )
 }
